04456a325f0f33811bde55cebbf0b12b25fe42d5
JHKIM <kjh123qwa@gmail.com>
2021-02-01 05:04:47 +0900

    salary... wlb... culrture... possibility... management... clear!!

--- recommend/views.py
+++ recommend/views.py
@@ -294,7 +294,13 @@ def resultJob(request, kind):
     elif kind == 'skill':
         df = pd.DataFrame(list(jobdict.objects.all().values()))
         df = df.dropna(axis=0)
-        df
+        wordDict2 = {
+            '복지 및 급여': 'salary',
+            '업무와 삶의 균형': 'wlb',
+            '사내문화': 'culture',
+            '승진 기회 및 가능성': 'possibility',
+            '경영진': 'management',
+        }
         vectorizer = CountVectorizer()  # 피쳐 벡터화
         feature_vector_job = vectorizer.fit_transform(df['skill'])  # 피쳐 벡터 행렬
         feature_job = vectorizer.get_feature_names()
@@ -355,8 +361,8 @@ def resultJob(request, kind):
             for (index, item) in df.iterrows():
                 reviewScore = round(
                     0.2 * (item['reviewCount'] if item['reviewCount'] < 10 else 10), 1)
-                totalScroe.append(round((item['similarity'] * 20) + reviewScore + item['average'] + ((item[first] if first != None else 0) * 0.6) + (
-                    (item[second] if second != None else 0) * 0.4) + ((item[third] if third != None else 0) * 0.2), 2))
+                totalScroe.append(round((item['similarity'] * 20) + reviewScore + item['average'] + ((item[wordDict2[first]] if first != None else 0) * 0.6) + (
+                    (item[wordDict2[second]] if second != None else 0) * 0.4) + ((item[wordDict2[third]] if third != None else 0) * 0.2), 2))
             df_finals[idx]['totalScore'] = totalScroe
             df_finals[idx].sort_values(
                 by=['totalScore'], axis=0, ascending=False, inplace=True)
@@ -575,7 +581,6 @@ def wanna_job(request):
         {'id': 'publish', 'name': '출판/편집디자인'}, {'id': 'pd',
                                                 'name': '방송연출/PD/감독'}, {'id': 'cs', 'name': '고객지원/CS'},
         {'id': 'culture', 'name': '조직문화'},
-<<<<<<< HEAD
         {'id': 'crm', 'name': 'CRM'}, {'id': 'maintenance', 'name': '유지/수리/정비'}, {'id': 'tech', 'name': '기술영업'}, {'id': 'product', 'name': '제품/산업디자인'}]}
     return render(request, 'infojob.html', context2)
 
@@ -612,51 +617,3 @@ def wanna_job(request):
 # # print(jobdict.objects.all())'
 # # django.core.exceptions.ImproperlyConfigured: Requested setting INSTALLED_APPS, but settings are not configured.
 # # You must either define the environment variable DJANGO_SETTINGS_MODULE or call settings.configure() before accessing settings.
-=======
-        {'id': 'crm', 'name': 'CRM'}, {'id': 'maintenance', 'name': '유지/수리/정비'},{'id':'tech','name':'기술영업'}, {'id': 'product', 'name': '제품/산업디자인'}]}
-    return render(request,'infojob.html',context2)
-
-def sec_result(requese):
-    #  파일 로드 및 불필요한 스킬 제거
-    job=['web','se']#사용자가 선택한 잡 리스트 html에서 가져오기
-    skillPerJob = pd.read_csv('skillPerJob.csv', index_col="index")
-    skillPerJob = skillPerJob.drop(['WEB', 'App', 'DB', 'Data'])
-
-    # 최다 언급 skill 기준 정렬
-    df = pd.DataFrame(skillPerJob[job])
-    df = df.drop(df[df[job] == 0].index)
-    df = df.sort_values(by=job, ascending=False)
-    print("공고에서 언급한 기술 갯수 : ", df.shape[0])
-
-    # 각 기술별 비율 컬럼 추가
-    df_sum = df[job].sum()
-    df['비율'] = df.apply(lambda x: round(x / df_sum * 100, 2))
-
-    # 기술 + 비율 딕셔너리화 : 상위 10개 추출
-    df = df.head(10)
-    top_skil = dict(zip(list(df.index), df['비율'].to_list()))
-    context3={'top_skill':top_skil}
-    return render(requese,'result2.html',context3)
-def readCsv(address):
-    df=pd.read_csv(address)
-    return df
-# address 파라미터 적어줄때  => r'절대주소'
-
-test_df=readCsv(r'C:\Users\네리\Desktop\sba_re\4조\job_data_list_final.csv')
-print(test_df.columns)
-#col=index,jobId,company,title,job,skill,region,experience,intro
-#task,require,prefer,jobUrl
-test_df=test_df.fillna('')
-
-print(type(test_df['jobId'][0]))
-
-#test => titleId,company,title,job,skill
-def create():
-    for i in range(len(test_df)):
-        test.objects.create(titleId=test_df['jobId'][i], company=test_df['company'][i], title=test_df['title'][i], job=test_df['job'][i], skill=test_df['skill'][i])
-print(len(test_df))
-#model.objects.all()
-#print(jobdict.objects.all())'
-#django.core.exceptions.ImproperlyConfigured: Requested setting INSTALLED_APPS, but settings are not configured.
-# You must either define the environment variable DJANGO_SETTINGS_MODULE or call settings.configure() before accessing settings.
->>>>>>> 9a765101f12d88a8572722bb6bff7e04688dbf9f
