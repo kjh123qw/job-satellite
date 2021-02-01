54b54d4087d364ecac8dc999f25a80d8e274ecad
JHKIM <kjh123qwa@gmail.com>
2021-02-01 20:33:23 +0900

    oracle server first commit

--- JobSatellite/settings.py
+++ JobSatellite/settings.py
@@ -17,10 +17,10 @@ import cx_Oracle
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # lib_dir = r"C:\Users\dell\Documents\IT\ORACLE\instantclient_19_9"
-# lib_dir2 = r"\home\ubuntu\instantclient_19_9"
-lib_dir3 = "/home/ubuntu/instantclient_21_1"
+lib_dir2 = r"\home\ubuntu\instantclient_21_1"
+# lib_dir3 = "/home/ubuntu/instantclient_21_1"
 # try:
-cx_Oracle.init_oracle_client(lib_dir=lib_dir3)
+cx_Oracle.init_oracle_client(lib_dir=lib_dir2)
 # except Exception as err:
 #     print("Error connecting: cx_Oracle.init_oracle_client()")
 #     print(err)
