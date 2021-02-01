564270c147768007d34bb8587941e08acdb4ad59
JHKIM <kjh123qwa@gmail.com>
2021-02-01 21:01:02 +0900

    oracle server first commit

--- JobSatellite/settings.py
+++ JobSatellite/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # lib_dir = r"C:\Users\dell\Documents\IT\ORACLE\instantclient_19_9"
 # lib_dir2 = r"\home\ubuntu\instantclient_19_9"
-lib_dir3 = "/home/ubuntu/instantclient_19_9"
+lib_dir3 = "/home/ubuntu/opt/oracle/instantclient_19_9"
 # try:
 cx_Oracle.init_oracle_client(lib_dir=lib_dir3)
 # except Exception as err:
